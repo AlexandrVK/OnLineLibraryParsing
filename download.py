e7460a5fa4a4ca1c37b393df80b262414a955a01
AlexandrVK <alexandrvk@gmail.com>
2023-04-12 19:33:05 +0300

    download books with titles

--- download.py
+++ download.py
@@ -1,18 +1,41 @@
-import pathlib
+from pathlib import Path
 import requests
+import os
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
 
-def books_downloader(filename,path,url,params=""):
-    pathlib.Path(path).mkdir(parents=True, exist_ok=True) 
-    response = requests.get(url,params=params)
-    response.raise_for_status()
-    with open(f"{path}/{filename}", 'w') as file:
-        file.write(response.text)
+def check_for_redirect(response):
+     if any(r.status_code == 302 for r in response.history):
+        raise requests.exceptions.HTTPError()
 
-path="books"
-# url = "https://tululu.org/txt.php?id=32168"
+def download_txt(url, filename, folder='books/'):
+    """Функция для скачивания текстовых файлов.
+    Args:
+        url (str): Cсылка на текст, который хочется скачать.
+        filename (str): Имя файла, с которым сохранять.
+        folder (str): Папка, куда сохранять.
+    Returns:
+        str: Путь до файла, куда сохранён текст.
+    """
+    Path(folder).mkdir(parents=True, exist_ok=True)
+    try:  
+        response = requests.get(url)
+        check_for_redirect(response)
+        response.raise_for_status()
+        filepath=os.path.join(folder,f"{sanitize_filename(filename)}.txt")
+        with open(filepath, 'w') as file:
+            file.write(response.text)
+        return filepath  
+    except: 
+        return "No FILE!!!"
 
-for book in range(10):
-    url="https://tululu.org/txt.php"
-    params={"id": f"{book+1}"}
-    filename =f"id{book+1}"
-    books_downloader(filename,path,url,params)
+for book in range(1,10):
+    try:  
+        response = requests.get(f"https://tululu.org/b{book}")
+        check_for_redirect(response)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, 'lxml')
+        title, autor = str.split(soup.find('h1').text,"::")
+        print(f"{book}. ",download_txt(f"https://tululu.org/txt.php?id={book}", title.strip()))
+    except: 
+        pass
