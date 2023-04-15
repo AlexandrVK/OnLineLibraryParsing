e7471572cda474da2580aadf566969cf6726deb6
AlexandrVK <alexandrvk@gmail.com>
2023-04-15 16:51:24 +0300

    Добавлен параметр к URL

--- download.py
+++ download.py
@@ -1,21 +1,23 @@
 import argparse
 from pathlib import Path
-import pprint
 from urllib.parse import urljoin, urlsplit,unquote
 import requests
 import os
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
+import urllib.parse
+
 
 def check_for_redirect(response):
      
      if not len(response.history) == 0:
         raise requests.exceptions.HTTPError()
 
-def download_txt(url, filename, folder='books/'):
+def download_txt(url, payload, filename, folder='books/'):
     """Функция для скачивания текстовых файлов.
     Args:
-        url (str): Cсылка на текст, который хочется скачать.
+        url (str): Cсылка на текст, который требуется скачать.
+        payload: параметры URL.
         filename (str): Имя файла, с которым сохранять.
         folder (str): Папка, куда сохранять. (по умолчанию "books" )
     Returns:
@@ -23,7 +25,8 @@ def download_txt(url, filename, folder='books/'):
     """
     Path(folder).mkdir(parents=True, exist_ok=True)
     try:  
-        response = requests.get(url)
+        
+        response = requests.get(url,params = payload )
         check_for_redirect(response)
         response.raise_for_status()
         filepath=os.path.join(folder,f"{sanitize_filename(filename)}.txt")
@@ -36,8 +39,8 @@ def download_txt(url, filename, folder='books/'):
 def download_image(url,filename,folder='images/'):
     """Скачивает изображения по указанному URL.
     Args:
-        filename (str): имя файла, которое присвоится скачанному файлу
-        url (str): URL для скачивания файла
+        filename (str): имя файла, которое присвоится скачанному файлу.
+        url (str): URL для скачивания файла.
         folder (str): Папка, куда сохранять. (по умолчанию "images" )
     """
     Path(folder).mkdir(parents=True, exist_ok=True) 
@@ -89,8 +92,9 @@ def main():
 
             parsed_page_data=parse_book_page(soup)
 
-                   
-            download_txt(f"https://tululu.org/txt.php?id={book}", parsed_page_data["Название"])
+          
+            params = urllib.parse.urlencode({"id": book})
+            download_txt(f"https://tululu.org/txt.php", {"id": book}, parsed_page_data["Название"])
 
             url = urljoin("https://tululu.org/",soup.find(class_ ='bookimage').find('img')['src'])
             filename =  unquote(urlsplit(url).path.split("/")[-1])
