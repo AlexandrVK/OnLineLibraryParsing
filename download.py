28cd9b387e20a8ac8075f0cce89f186245f61a73
AlexandrVK <alexandrvk@gmail.com>
2023-04-15 17:11:02 +0300

    Откорректирована обработка исключений

--- download.py
+++ download.py
@@ -7,7 +7,6 @@ from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
 
 
-
 def check_for_redirect(response):
      
      if not len(response.history) == 0:
@@ -24,19 +23,15 @@ def download_txt(url, payload, filename, folder='books/'):
         str: Путь до файла, куда сохранён текст.
     """
     Path(folder).mkdir(parents=True, exist_ok=True)
-    try:  
-        
-        response = requests.get(url,params = payload )
-        check_for_redirect(response)
-        response.raise_for_status()
-        filepath=os.path.join(folder,f"{sanitize_filename(filename)}.txt")
-        with open(filepath, 'w') as file:
-            file.write(response.text)
-        return filepath  
-    except: 
-        return "No FILE!!!"
+    response = requests.get(url,params = payload )
+    check_for_redirect(response)
+    response.raise_for_status()
+    filepath=os.path.join(folder,f"{sanitize_filename(filename)}.txt")
+    with open(filepath, "w") as file:
+        file.write(response.text)
+    return filepath  
 
-def download_image(url,filename,folder='images/'):
+def download_image(url,filename,folder="images/"):
     """Скачивает изображения по указанному URL.
     Args:
         filename (str): имя файла, которое присвоится скачанному файлу.
@@ -44,15 +39,12 @@ def download_image(url,filename,folder='images/'):
         folder (str): Папка, куда сохранять. (по умолчанию "images" )
     """
     Path(folder).mkdir(parents=True, exist_ok=True) 
-    try: 
-        response = requests.get(url)
-        check_for_redirect(response)
-        response.raise_for_status()
-        with open(os.path.join("images",filename), 'wb') as file:
-            file.write(response.content)
-        return True    
-    except: 
-        return False
+    response = requests.get(url)
+    check_for_redirect(response)
+    response.raise_for_status()
+    with open(os.path.join("images",filename), "wb") as file:
+        file.write(response.content)
+    return True    
 
 def parse_book_page(soup):
     """Возвращает словарь со всеми данными о книге.
@@ -61,11 +53,11 @@ def parse_book_page(soup):
     Returns:
         Cловарь со всеми данными о книге:
     """ 
-    title, author = [string.strip() for string in str.split(soup.find('h1').text, "::")]
+    title, author = [string.strip() for string in str.split(soup.find("h1").text, "::")]
 
-    genres = [string.text.strip() for string in soup.find('span',class_='d_book').find_all('a')]
+    genres = [string.text.strip() for string in soup.find("span",class_="d_book").find_all("a")]
     
-    comments = [string.find('span',class_ ='black' ).text.strip() for string in soup.find_all('div',class_ ='texts')]
+    comments = [string.find("span",class_ ="black" ).text.strip() for string in soup.find_all("div",class_ ="texts")]
             
     return {"title" : title, "author" : author, "genres" : genres, "comments" : comments } 
 
@@ -80,27 +72,21 @@ def main():
     
     for book in range(args.start_id,args.end_id):
         try:  
-            
             response = requests.get(f"https://tululu.org/b{book}/")
             check_for_redirect(response)
             response.raise_for_status()
-            soup = BeautifulSoup(response.text, 'lxml')
+            soup = BeautifulSoup(response.text, "lxml")
 
             parsed_page_data=parse_book_page(soup)
-
-          
             
             download_txt(f"https://tululu.org/txt.php", {"id": book}, parsed_page_data["title"])
 
-            url = urljoin("https://tululu.org/",soup.find(class_ ='bookimage').find('img')['src'])
+            url = urljoin("https://tululu.org/",soup.find(class_ ="bookimage").find("img")["src"])
             filename =  unquote(urlsplit(url).path.split("/")[-1])
             
             download_image(url,filename)
-           
-             
-
         except: 
             pass
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
